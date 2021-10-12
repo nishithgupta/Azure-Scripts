421bb408380935c6befaa36d28b94cbfa40c66ce
nishithgupta <65109153+nishithgupta@users.noreply.github.com>
2021-10-12 12:28:47 +0530

    Update and rename code.sh to create-s2s-vpn.sh

--- create-s2s-vpn.sh
+++ create-s2s-vpn.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # Define variables
 RGName=HubRG                # Name of the Resource group to be created
 KVName=KVforAuth            # A globally unique name of the Keyvault to be created
@@ -11,6 +13,7 @@ az account set --subscription $SubscriptionId
 
 # Creat Azure Resource Group. Save resource id into a variable.
 RGResourceId=$(az group create --name $RGName --location centralindia --query id --output tsv)
+
 # az config set defaults.group=$RGName
 
 # Register the Key Vault resource provider. Create Key Vault that uses Azure RBAC model. Save Keyvault resource id into a variable
@@ -60,25 +63,55 @@ VpnGWVpntype=RouteBased                 # Type of VPN routing. Other possible va
 VpnGWSku=Basic                          # See https://docs.microsoft.com/en-us/azure/vpn-gateway/vpn-gateway-about-vpn-gateway-settings#gwsku and https://docs.microsoft.com/en-us/cli/azure/network/vnet-gateway?view=azure-cli-latest#az_network_vnet_gateway_create
 # VpnGWGen=Generation1/Generation2      use --vpn-gateway-generation for any SKU other than Basic and VpnGw1
 LocalNWGW=HubLocalNWGateway             # Name of the local (on-prem) network gateway
-LocalNWGWPip=122.177.75.17              # Public IP address of on-prem router/vpn device.
+LocalNWGWPip=                           # Public IP address of on-prem router/vpn device.
 LocalNWGWPf=192.168.0.0/16              # Address space of the local (on-prem) network.
 VPNCon=Hub2OnPS2SVpnConnection          # Name of the VPN connection
 SharedKey=ADGJLPIYRWZCBM135792468       # A Shared IPsecv2 key. This will be needed on on-premise vpn device also.
 
 # Create vNet and Subnet
-az network vnet create --resource-group $RGName --name $vNet --address-prefix $vNetPf --subnet-name $Subnet --subnet-prefix $SubnetPf
+az network vnet create \
+  --resource-group $RGName \
+  --name $vNet \
+  --address-prefix $vNetPf \
+  --subnet-name $Subnet \
+  --subnet-prefix $SubnetPf
 
 # Create Gateway subnet. Name of the Gateway subnet will always be GatewaySubnet
-az network vnet subnet create --resource-group $RGName --vnet-name $vNet --address-prefix $GWSubnetPf --name GatewaySubnet
+az network vnet subnet create \
+  --resource-group $RGName \
+  --vnet-name $vNet \
+  --address-prefix $GWSubnetPf \
+  --name GatewaySubnet
 
 # Create public IP for Virtual network gateway
-az network public-ip create --name $PipName --resource-group $RGName --allocation-method Dynamic
+az network public-ip create \
+  --name $PipName \
+  --resource-group $RGName \
+  --allocation-method Dynamic
 
 # Create Virtual Network Gateway of type VPN
-az network vnet-gateway create --resource-group $RGName --name $VpnGW --public-ip-address $PipName --vnet $vNet --gateway-type $VpnGWType --vpn-type $VpnGWVpntype --sku $VpnGWSku --no-wait
+az network vnet-gateway create \
+  --resource-group $RGName \
+  --name $VpnGW \
+  --public-ip-address $PipName \
+  --vnet $vNet \
+  --gateway-type $VpnGWType \
+  --vpn-type $VpnGWVpntype \
+  --sku $VpnGWSku \
+  --no-wait
 
 # Create Local network gateway
-az network local-gateway create --gateway-ip-address $LocalNWGWPip --name $LocalNWGW --resource-group $RGName --local-address-prefixes $LocalNWGWPf
+az network local-gateway create \
+  --gateway-ip-address $LocalNWGWPip \
+  --name $LocalNWGW \
+  --resource-group $RGName \
+  --local-address-prefixes $LocalNWGWPf
 
 # Create a logical VPN connection 
-az network vpn-connection create --name $VPNCon --resource-group $RGName --vnet-gateway1 $VpnGW --location centralindia --shared-key abc123 --local-gateway2 $LocalNWGW
+az network vpn-connection create \
+  --name $VPNCon \
+  --resource-group $RGName \
+  --vnet-gateway1 $VpnGW \
+  --location centralindia \
+  --shared-key abc123 \
+  --local-gateway2 $LocalNWGW
