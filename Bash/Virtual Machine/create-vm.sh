4f5d449acb69b2de9006a4446f119a4892479149
Nishith Gupta <65109153+nishithgupta@users.noreply.github.com>
2021-11-21 20:08:13 +0530

    Update create-vm.sh

--- Bash/Virtual Machine/create-vm.sh
+++ Bash/Virtual Machine/create-vm.sh
@@ -1,22 +1,27 @@
 #!/bin/bash
 
-# Update for your admin password
-VMName=AZDDC01
-VMLocation=
-VMImage=
-VMSize=
-VMOSDiskType=
-AdminUser=CZAdmin
-AdminPassword=Divergent@123
-RGName=CtxInfraRG
-vNet=CtxInfravNet
-Subnet=CtxInfraSubnet
-PipName=AZDDC01-pip
-NSGName=AZDDC01-NSG
-NicName=AZDDC01-NIC
+# Define variables. A detailed description of these variables in available in README.md file
+VMName=MyVM1
+VMLocation=centralindia
+VMImage=win2019datacenter OR MicrosoftWindowsServer:WindowsServer:2019-datacenter-gensecond:latest
+VMSize=Standard_B2s
+VMOSDiskType=Premium_LRS or StandardSSD_LRS or Standard_LRS or UltraSSD_LRS or Premium_ZRS or StandardSSD_ZRS
+VMOSDiskName=MyVM01-OSDisk
+VMOSDiskSize=200
+DataDisk1Size=10
+DataDisk2Size=20
+AdminUser=AzAdmin
+AdminPassword=P@$$w0r6@123
+RGName=MyResourceGroup
+vNet=MyvNet
+Subnet=MySubnet
+PipName=MyVM1-pip
+PipSKU=Basic or Standard
+NSGName=MyVM1-NSG
+NicName=MyVM1-NIC
 
 # Create a public IP address.
-az network public-ip create --resource-group $RGName --name $PipName
+az network public-ip create --resource-group $RGName --name $PipName --sku $PipSKU --allocation-method Static
 
 # Create a network security group.
 az network nsg create --resource-group $RGName --name $NSGName
@@ -32,13 +37,21 @@ az network nic create \
 
 # Create a virtual machine. 
 az vm create \
-    --resource-group myResourceGroup \
+    --resource-group $RGName \
     --name $VMName \
-    --location westeurope \
+    --location $VMLocation \
     --nics $NicName \
-    --image win2016datacenter \
+    --image $VMImage \
+    --size $VMSize \
+    --os-disk-name $VMOSDiskName \
+    --os-disk-size-gb $VMOSDiskSize \
+    --storage-sku $VMOSDiskType \
+    --data-disk-sizes-gb $DataDisk1Size $DataDisk2Size \
     --admin-username $AdminUser \
     --admin-password $AdminPassword
+    --enable-auto-update
 
 # Open port 3389 to allow RDP traffic to host.
 az vm open-port --port 3389 --resource-group $RGName --name $VMName
+
+# End of Script
